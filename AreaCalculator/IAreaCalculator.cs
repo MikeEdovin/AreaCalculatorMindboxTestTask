793cf585d91c9c03e59f5b1b16de92b0b716cf68
MikeEdovin <edovinmihail@gmail.com>
2023-03-21 14:15:25 +0300

    added summary

--- AreaCalculator/IAreaCalculator.cs
+++ AreaCalculator/IAreaCalculator.cs
@@ -2,6 +2,10 @@
 
 namespace AreaCalculatorProject
 {
+    /// <summary>
+    /// Calculates the area of the transmitted shape
+    /// may throw an ArgumentException if the shape with the specified parameters does not exist
+    /// </summary>
     public interface IAreaCalculator
     {
         double Calculate(Shape shape);
