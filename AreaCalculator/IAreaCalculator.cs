4542589e8742fdbb6f94ab296e75e28183160e56
MikeEdovin <edovinmihail@gmail.com>
2023-03-21 14:30:03 +0300

    summary

--- AreaCalculator/IAreaCalculator.cs
+++ AreaCalculator/IAreaCalculator.cs
@@ -5,6 +5,8 @@ namespace AreaCalculatorProject
     /// <summary>
     /// Calculates the area of the transmitted shape
     /// may throw an ArgumentException if the shape with the specified parameters does not exist
+    /// uses metric number system
+    /// rounds result to the second digit after the dot
     /// </summary>
     public interface IAreaCalculator
     {
